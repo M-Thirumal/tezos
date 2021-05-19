a794db551acd052630a754ccff3f08dead73f8ed
Thirumal <m.thirumal@hotmail.com>
2021-05-19 21:59:57 +0530

    full code

---
+++ account.js
@@ -0,0 +1,25 @@
+let FAUCET_KEY;
+export default FAUCET_KEY = {
+    "mnemonic": [
+      "humble",
+      "elephant",
+      "entry",
+      "retire",
+      "vacuum",
+      "raven",
+      "base",
+      "field",
+      "rude",
+      "lazy",
+      "victory",
+      "already",
+      "cross",
+      "cherry",
+      "grunt"
+    ],
+    "secret": "e44e2ed981220ebe4a37e0303d6d13db8dbe0a80",
+    "amount": "2977564844",
+    "pkh": "tz1hjAwHEKaLzCp7mLHgRcYcGX96wZm6XwbT",
+    "password": "ra265e9pmM",
+    "email": "luwnjitm.xwtgqgvx@tezos.example.org"
+  }
